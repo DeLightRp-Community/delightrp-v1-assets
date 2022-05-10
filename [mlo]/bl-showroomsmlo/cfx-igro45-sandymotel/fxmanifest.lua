b848810dd91bd7fa5a5c8a3a27ac8dae882d7347
Server <hkarimi561@gmail.com>
2022-05-10 06:13:19 -0400

    update mappings

---
+++ [mlo]/bl-showroomsmlo/cfx-igro45-sandymotel/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Igro45'
+description 'Sandy Shores Motel'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+dependencies {
+    '/server:4960',
+    '/gameBuild:2189'
+}
+
+escrow_ignore {
+    'stream/**/',
+}
+
+dependency '/assetpacks'
