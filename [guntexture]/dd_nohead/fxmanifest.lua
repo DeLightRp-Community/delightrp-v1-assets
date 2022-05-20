5f54c579a67f4719aef1a5f1b5632d08b81f7474
Server <hkarimi561@gmail.com>
2022-05-20 07:54:01 -0400

    add cloths

---
+++ [guntexture]/dd_nohead/fxmanifest.lua
@@ -0,0 +1,43 @@
+fx_version 'adamant'
+game 'gta5'
+
+--SILAH DAMAGELERI KAFAYA TEK ATMAZ GOVDEYLE AYNI VURUR
+--DEADLİGHT
+
+files {
+  'data/weapons.meta',
+  'data/weaponcombatpdw.meta',
+  'data/weaponcompactrifle.meta',
+  'data/weaponheavypistol.meta',
+  'data/weaponmachinepistol.meta',
+  'data/weaponminismg.meta',
+  'data/weaponrevolver.meta',
+  'data/weapons_carbinerifle_mk2.meta',
+  'data/weapons_doubleaction.meta',
+  'data/weapons_pistol_mk2.meta',
+  'data/weapons_revolver_mk2.meta',
+  'data/weapons_smg_mk2.meta',
+  'data/weapons_snspistol_mk2.meta',
+  'data/weapons_specialcarbine_mk2.meta',
+  'data/weaponsnspistol.meta',
+  'data/weaponspecialcarbine.meta',
+  'data/weaponvintagepistol.meta',
+}
+
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponcombatpdw.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponcompactrifle.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponheavypistol.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponmachinepistol.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponminismg.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponrevolver.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_carbinerifle_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_doubleaction.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_pistol_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_revolver_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_smg_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_snspistol_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_specialcarbine_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponsnspistol.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponspecialcarbine.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponvintagepistol.meta'
